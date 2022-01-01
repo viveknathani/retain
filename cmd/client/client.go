4ac539330ef073868941367cc9fa5fb0ddc988f5
viveknathani <viveknathani2402@gmail.com>
2022-01-01 15:06:28 +0530

    feat: customSplit, SAVE on ^C
    customSplit is needed because separating by space is ineffective if
    the double quotes are being used to accept space separated inputs.

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"os/signal"
+	"reflect"
+	"syscall"
 
 	"github.com/viveknathani/retain/protocol"
 )
@@ -21,23 +23,57 @@ func main() {
 	connection, err := net.Dial("tcp", *host+":"+fmt.Sprint(*port))
 	handleError("client main: ", err)
 
-	for {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	done := make(chan bool, 1)
+	go waitForSignal(connection, sig, done)
 
-		fmt.Print("> ")
-		userReader := bufio.NewReader(os.Stdin)
-		userInput, err := userReader.ReadBytes('\n')
-		handleError("client main: ", err)
+	go func() {
 
-		arr := bytes.Split(userInput[0:len(userInput)-1], []byte(" "))
-		_, err = connection.Write([]byte(protocol.Encode(arr)))
-		handleError("client main: ", err)
+		for {
 
-		buffer := make([]byte, 65535)
-		bytesRead, err := connection.Read(buffer)
-		handleError("client main: ", err)
-		buffer = buffer[0:bytesRead]
-		fmt.Printf(">> %s\n", protocol.Decode(buffer))
-	}
+			fmt.Print("> ")
+			userReader := bufio.NewReader(os.Stdin)
+			userInput, err := userReader.ReadBytes('\n')
+			handleError("client main: ", err)
+
+			arr := customSplit(userInput[0 : len(userInput)-1])
+			_, err = connection.Write([]byte(protocol.Encode(arr)))
+			handleError("client main: ", err)
+
+			buffer := make([]byte, 65535)
+			bytesRead, err := connection.Read(buffer)
+			handleError("client main: ", err)
+			buffer = buffer[0:bytesRead]
+			decoded := protocol.Decode(buffer)
+
+			switch decoded.(type) {
+			case []interface{}:
+				list := reflect.ValueOf(decoded)
+				for i := 0; i < list.Len(); i++ {
+					fmt.Printf(">>(%d) %s\n", i+1, list.Index(i))
+				}
+				break
+			default:
+				fmt.Printf(">> %s\n", decoded)
+			}
+		}
+	}()
+
+	<-done
+	fmt.Println("goodbye!")
+}
+
+func waitForSignal(connection net.Conn, sig <-chan os.Signal, done chan<- bool) {
+
+	captured := <-sig
+	arr := make([][]byte, 0)
+	arr = append(arr, []byte("SAVE"))
+	_, err := connection.Write([]byte(protocol.Encode(arr)))
+	handleError("waitForSignal, upon saving:", err)
+	fmt.Println()
+	fmt.Println(captured)
+	done <- true
 }
 
 func handleError(text string, err error) {
@@ -45,4 +81,47 @@ func handleError(text string, err error) {
 	if err != nil {
 		log.Fatal(text, err)
 	}
+}
+
+func customSplit(userInput []byte) [][]byte {
+
+	result := make([][]byte, 0)
+
+	i := 0
+	// get the first segment, the command
+	temp := make([]byte, 0)
+	for ; i < len(userInput); i++ {
+
+		if userInput[i] == ' ' {
+			i++
+			break
+		}
+
+		temp = append(temp, userInput[i])
+	}
+	result = append(result, temp)
+
+	captureUntilNextQuote := false
+
+	temp = make([]byte, 0)
+	for ; i < len(userInput); i++ {
+
+		if userInput[i] == '"' {
+			captureUntilNextQuote = !captureUntilNextQuote
+			continue
+		}
+
+		if userInput[i] == ' ' && !captureUntilNextQuote {
+			result = append(result, temp)
+			temp = make([]byte, 0)
+			continue
+		}
+
+		temp = append(temp, userInput[i])
+	}
+
+	if len(temp) != 0 {
+		result = append(result, temp)
+	}
+	return result
 }
