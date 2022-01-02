edf8a55b61750be1c0d1630b0a006c7798af02b5
viveknathani <viveknathani2402@gmail.com>
2022-01-02 14:14:50 +0530

    feat: use colors, log client details

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,39 +4,106 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"log"
+	"io"
 	"net"
+	"os"
+	"runtime"
+	"syscall"
 
 	"github.com/viveknathani/retain/protocol"
 	"github.com/viveknathani/retain/store"
 )
 
+const (
+	colorReset  = "\033[0m"
+	colorRed    = "\033[31m"
+	colorGreen  = "\033[32m"
+	colorYellow = "\033[33m"
+	colorBlue   = "\033[34m"
+	colorPurple = "\033[35m"
+	colorCyan   = "\033[36m"
+	colorWhite  = "\033[37m"
+	colorPink   = "\033[38;5;13m"
+)
+
+func printColor(colorName string) {
+
+	os := runtime.GOOS
+	if os != "windows" {
+		fmt.Print(colorName)
+	}
+}
+
 func serve(store *store.Storage, connection net.Conn) {
 
 	defer connection.Close()
 
+	address := connection.RemoteAddr().String()
+	printColor(colorGreen)
+	fmt.Printf("new client => %s\n", address)
+	printColor(colorReset)
+
 	for {
 		buffer := make([]byte, 65535)
 		bytesRead, err := connection.Read(buffer)
-
-		if err != nil {
-			fmt.Println("serve:", err)
-			return
+		if handleErrorWhileServing(address, err) {
+			break
 		}
 
 		buffer = buffer[0:bytesRead]
 		arr := protocol.Decode(buffer)
-		response := executeCommand(store, arr.([]interface{}))
+		response := executeCommand(store, arr.([]interface{}), address)
 		_, err = connection.Write(response)
-		handleError("serve: ", err)
+		if handleErrorWhileServing(address, err) {
+			break
+		}
 	}
 }
 
-func executeCommand(store *store.Storage, respArray []interface{}) protocol.RespEncodedString {
+func handleErrorWhileServing(address string, err error) bool {
+
+	if err == nil {
+		return false
+	}
+
+	printColor(colorRed)
+	fmt.Printf("[%s] > ", address)
+	if checkIfClientLeft(err) {
+		fmt.Printf("(left)\n")
+		printColor(colorReset)
+		return true
+	}
+
+	if checkIfWeLostConnection(err) {
+		fmt.Printf("(ECONNRESET)\n")
+		printColor(colorReset)
+		return true
+	}
+
+	fmt.Printf("%s\n", err.Error())
+	printColor(colorReset)
+	return true
+}
+
+func checkIfClientLeft(err error) bool {
+	return errors.Is(err, io.EOF)
+}
+
+func checkIfWeLostConnection(err error) bool {
+	return errors.Is(err, syscall.ECONNRESET)
+}
+
+func executeCommand(store *store.Storage, respArray []interface{}, address string) protocol.RespEncodedString {
 
 	response := ""
 	errorMessage := protocol.Encode(errors.New("invalid command syntax"))
-	switch string(respArray[0].([]byte)) {
+
+	command := string(respArray[0].([]byte))
+	printColor(colorYellow)
+	fmt.Printf("[%s] > request for %s\n", address, command)
+	printColor(colorReset)
+
+	switch command {
 
 	case "PING":
 		if len(respArray) > 1 {
@@ -138,7 +205,11 @@ func main() {
 	listener, err := net.Listen("tcp", *host+":"+fmt.Sprint(*port))
 	handleError("server main: ", err)
 
-	storage := store.New()
+	storage, loadedFromDisk := store.New()
+
+	if loadedFromDisk {
+		fmt.Println("loaded from disk (retain.db)")
+	}
 
 	for {
 		connection, err := listener.Accept()
@@ -150,6 +221,9 @@ func main() {
 func handleError(text string, err error) {
 
 	if err != nil {
-		log.Fatal(text, err)
+		printColor(colorRed)
+		fmt.Println(text, err)
+		printColor(colorReset)
+		os.Exit(1)
 	}
 }
